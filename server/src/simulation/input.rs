3841829d1ef357c59c189aacd2ef48694dd167f7
Snowdeenn <legoffelouan05@gmail.com>
2026-05-30 22:21:33 +0200

    Input state retirer

--- server/src/simulation/input.rs
+++ server/src/simulation/input.rs
@@ -3,11 +3,3 @@ use shared::protocol::InputPacket;
 
 #[derive(Debug, Default)]
 pub struct InputQueue(pub Vec<InputPacket>);
-
-#[derive(Debug, Default, Clone, Copy)]
-pub struct InputState {
-    pub move_dir: [f32; 2],
-    pub aim_dir:  [f32; 2],
-    pub dash:     bool,
-    pub spell:    Option<u8>,
-}

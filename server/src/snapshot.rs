c4afec1b800168c0ab5caf56d421a212ad6effe0
Snowdeenn <legoffelouan05@gmail.com>
2026-06-23 19:36:52 +0200

    ajout du build des projectiles dans les snapshot

--- server/src/snapshot.rs
+++ server/src/snapshot.rs
@@ -1,21 +1,26 @@
 use std::collections::HashMap;
 
-use crate::simulation::components::{Active, EntityId, Health, IA, Player, Position};
-use crate::simulation::wave::{WaveManager, WaveState as SimWaveState};
-use legion::*;
-use shared::protocol::{EntityKind, EntityState, StateSnapshot, WaveInfo, WaveState, PlayerInfo};
 use crate::Coin;
 use crate::PlayerGold;
+use crate::simulation::components::{Active, EntityId, Health, IA, Player, Position, Projectile};
+use crate::simulation::wave::{WaveManager, WaveState as SimWaveState};
+use legion::*;
+use shared::protocol::{EntityKind, EntityState, PlayerInfo, StateSnapshot, WaveInfo, WaveState};
 
-pub fn build_snapshot(player_id: u64, world: &mut World, resources: &Resources, tick_id: u64) -> StateSnapshot {
+pub fn build_snapshot(
+    player_id: u64,
+    world: &mut World,
+    resources: &Resources,
+    tick_id: u64,
+) -> StateSnapshot {
     let wave_info = build_wave_info(resources);
     let entities = build_entities(world);
-    
+
     StateSnapshot {
         tick_id,
         entities,
         wave_info,
-        player_info: build_player_info(player_id,  world, resources),
+        player_info: build_player_info(player_id, world, resources),
     }
 }
 
@@ -47,7 +52,9 @@ fn build_entities(world: &World) -> Vec<EntityState> {
         let mut player_query = <(&EntityId, &Position, &Health, &Active)>::query()
             .filter(component::<Player>() & component::<Active>());
         for (id, pos, health, active) in player_query.iter(world) {
-            if !active.0 { continue; }
+            if !active.0 {
+                continue;
+            }
             entities.push(EntityState {
                 entity_id: id.0,
                 position: [pos.x as f32, pos.y as f32],
@@ -63,7 +70,9 @@ fn build_entities(world: &World) -> Vec<EntityState> {
         let mut ia_query = <(&EntityId, &Position, &Health, &Active)>::query()
             .filter(component::<IA>() & component::<Active>());
         for (id, pos, health, active) in ia_query.iter(world) {
-            if !active.0 { continue; }
+            if !active.0 {
+                continue;
+            }
             entities.push(EntityState {
                 entity_id: id.0,
                 position: [pos.x as f32, pos.y as f32],
@@ -76,10 +85,12 @@ fn build_entities(world: &World) -> Vec<EntityState> {
 
     // coins
     {
-        let mut coin_query =
-            <(&EntityId, &Position, &Active)>::query().filter(component::<Coin>() & component::<Active>());
+        let mut coin_query = <(&EntityId, &Position, &Active)>::query()
+            .filter(component::<Coin>() & component::<Active>());
         for (id, pos, active) in coin_query.iter(world) {
-            if !active.0 { continue; }
+            if !active.0 {
+                continue;
+            }
             entities.push(EntityState {
                 entity_id: id.0,
                 position: [pos.x as f32, pos.y as f32],
@@ -90,10 +101,32 @@ fn build_entities(world: &World) -> Vec<EntityState> {
         }
     }
 
+    // projectiles
+    {
+        let mut proj_query = <(&EntityId, &Position, &Active)>::query()
+            .filter(component::<Projectile>() & component::<Active>());
+        for (id, pos, active) in proj_query.iter(world) {
+            if !active.0 {
+                continue;
+            }
+            entities.push(EntityState {
+                entity_id: id.0,
+                position: [pos.x as f32, pos.y as f32],
+                health: 0.0,
+                max_health: 0.0,
+                entity_kind: EntityKind::Projectile,
+            });
+        }
+    }
+
     entities
 }
 
-fn build_player_info(player_id: u64, world: &mut World, resources: &Resources) -> Option<PlayerInfo> {
+fn build_player_info(
+    player_id: u64,
+    world: &mut World,
+    resources: &Resources,
+) -> Option<PlayerInfo> {
     let players_entities = resources.get::<HashMap<u64, Entity>>().unwrap();
     let entity = players_entities.get(&player_id)?;
 
@@ -104,9 +137,8 @@ fn build_player_info(player_id: u64, world: &mut World, resources: &Resources) -
     let gold = player_gold_res.0.get(&player_id).cloned().unwrap_or(0);
 
     Some(PlayerInfo {
-        health:     health.hp as f32,
+        health: health.hp as f32,
         max_health: 100.0,
         gold,
     })
 }
-

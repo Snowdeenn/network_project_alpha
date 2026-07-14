42af19fd8f7362cf41bc55f8e49344b3b05ef97f
Snowdeenn <legoffelouan05@gmail.com>
2026-07-14 21:45:06 +0200

    ajout type NodeId

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -1,26 +1,26 @@
 pub mod node;
-pub mod arena;
 pub mod context;
 pub mod layout;
 pub mod draw;
-pub mod texture;
-pub mod shader;
 pub mod event;
 pub mod tween;
 pub mod r#macro;
 pub mod input;
 pub mod output;
+pub mod provider;
 
 pub mod prelude {
     pub use crate::node::*;
-    pub use crate::arena::*;
     pub use crate::context::UiContext;
     pub use crate::layout::*;
     pub use crate::draw::*;
-    pub use crate::texture::*;
-    pub use crate::shader::*;
     pub use crate::event::*;
     pub use crate::tween::*;
     pub use crate::input::*;
     pub use crate::output::UIOutputEvent;
+    pub use crate::provider::*;
 }
+
+pub use shared::arena::{Arena, Id};
+use crate::node::UiNode;
+pub type NodeId = Id<UiNode>;

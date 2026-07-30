4b3179bfe49ae72a01d7b66f69d14a911b0a09e1
Snowdeenn <legoffelouan05@gmail.com>
2026-07-30 18:49:05 +0200

    ajout vertex et mesh

---
+++ prism/src/geometry/mesh.rs
@@ -0,0 +1,61 @@
+#[repr(C)]
+#[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct VertexColored {
+    pub pos: [f32; 2],
+    pub color: [f32; 4],
+}
+
+#[repr(C)]
+#[derive(Debug, Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+pub struct VertexTextured {
+    pub pos: [f32; 2],
+    pub uv: [f32; 2],
+    pub color: [f32; 4],
+}
+
+#[derive(Debug, Default, Clone)]
+pub struct RawMesh<V> {
+    vertices: Vec<V>,
+    indices: Vec<u32>,
+}
+
+impl<V: Copy + bytemuck::Pod> RawMesh<V> {
+    pub fn new() -> Self {
+        Self {
+            vertices: Vec::new(),
+            indices: Vec::new(),
+        }
+    }
+
+    pub fn with_capacity(verts: usize, index: usize) -> Self {
+        Self {
+            vertices: Vec::with_capacity(verts),
+            indices: Vec::with_capacity(index),
+        }
+    }
+
+    pub fn push_vertex(&mut self, v: V) -> u32 {
+        let idx = self.vertices.len();
+        self.vertices.push(v);
+        idx as u32
+    }
+
+    pub fn push_triangle(&mut self, a: u32, b: u32, c: u32) {
+        self.indices.push(a);
+        self.indices.push(b);
+        self.indices.push(c);
+    }
+
+    pub fn clear(&mut self) {
+        self.indices.clear();
+        self.vertices.clear();
+    }
+
+    pub fn vertices_bytes(&self) -> &[u8] {
+        bytemuck::cast_slice(&self.vertices)
+    }
+
+    pub fn indices_bytes(&self) -> &[u8] {
+        bytemuck::cast_slice(&self.indices)
+    }
+}

012c4e6eceff270a0245cd3191db49a0bf5b1551
Snowdeenn <legoffelouan05@gmail.com>
2026-07-30 18:49:27 +0200

    ajout forme geo de base

---
+++ prism/src/geometry/shape.rs
@@ -0,0 +1,51 @@
+pub enum Shape {
+    Quad {
+        pos: [f32; 2],
+        size: [f32; 2],
+        rotation: f32,
+        color: [f32; 4],
+        uv: Option<UvRect>,
+    },
+    Polygon {
+        center: [f32; 2],
+        sides: u32,
+        radius: f32,
+        color: [f32; 4],
+    },
+    SlantedQuad {
+        pos: [f32; 2],
+        size: [f32; 2],
+        skew: f32,      // décalage horizontal du haut par rapport au bas
+        color: [f32; 4],
+    },
+    RoundedRect {
+        pos: [f32; 2],
+        size: [f32; 2],
+        radius: f32,
+        segments: u32,  // nombre de segments par coin
+        color: [f32; 4],
+    },
+    Line {
+        start: [f32; 2],
+        end: [f32; 2],
+        thickness: f32,
+        color: [f32; 4],
+    },
+    Ring {
+        center: [f32; 2],
+        inner_r: f32,
+        outer_r: f32,
+        start_angle: f32,
+        end_angle: f32,
+        resolution: u32,
+        color: [f32; 4],
+    },
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct UvRect {
+    pub x: f32,
+    pub y: f32,
+    pub w: f32,
+    pub h: f32,
+}
